f8d656c66b7aa3bda6154a450304d92c847407d3
skyADMIN <546485299@qq.com>
2016-06-27 15:44:13 +0800

    init project by start.spring.io

---
+++ src/main/java/cn/edu/upc/yb/dormsearch/DormsearchApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.upc.yb.dormsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormsearchApplication.class, args);
+	}
+}
